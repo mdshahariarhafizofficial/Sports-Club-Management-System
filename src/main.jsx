f3b82c4f2265af5db96e0c8026ba54af0e3c6456
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-07-10 22:49:32 +0600

    Context Api and Firebase Auth setup done

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router'
 import router from './Router/Routes.jsx'
+import AuthProvider from './Context/AuthProvider.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+        <Toaster/>
+    </AuthProvider>
   </StrictMode>,
 )
