d50237be17477e02782ccda462b1ffd33d6e02f7
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-07-11 14:32:12 +0600

    Private Routes Added

---
+++ src/Routes/PrivateRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useAuth from '../Hooks/useAuth';
+import Loading from '../Pages/Loading/Loading';
+import { Navigate, useLocation } from 'react-router';
+
+const PrivateRoutes = ({children}) => {
+    const location = useLocation();
+    const {user, loading} = useAuth();
+    if (loading) {
+        return <Loading></Loading>
+    }
+    
+    if (!user) {
+       return  <Navigate to='/login' state={location.pathname}></Navigate>
+    }
+    return children;
+};
+
+export default PrivateRoutes;
