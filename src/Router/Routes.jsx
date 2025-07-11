184d2c78ca771437ec14d9ac060a6c6c97503c7d
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-07-12 00:27:08 +0600

    Custom Axios Hooks Added

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Layouts/Root";
 import Home from "../Pages/Home/Home";
-import Loader from "../Pages/Loading/Loader";
 import Login from "../Pages/Login/Login";
 import AuthLayout from "../Layouts/AuthLayout";
 import Register from "../Pages/Register/Register";
