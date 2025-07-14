40a68c17217a798f1a2bddf046633376b6b4e6b0
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-07-15 00:12:37 +0600

    Load Pending Booking Data

--- src/Pages/Dashboard/PendingBookings/PendingBookings.jsx
+++ src/Pages/Dashboard/PendingBookings/PendingBookings.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import useAuth from '../../../Hooks/useAuth';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const PendingBookings = () => {
+    const {user} = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    // Get Data
+    const {data: userBookings = [] } = useQuery({
+        queryKey: ['userBookings', user?.email, 'pending'],
+        queryFn: async()=>{
+            const res = await axiosSecure.get(`/bookings?email=${user?.email}&status=pending`);
+            return res.data;
+        }
+    });
+    console.log(userBookings);
+    
     return (
         <div>
             <h1>Pending Bookings</h1>
